2fa4de672ec354627bfab394a37ed6d64fdcabb4
jithesh <jithx0901@gmail.com>
2026-09-06 19:02:58 +0530

    Time: 228 ms (82.25%), Space: 19.5 MB (91.77%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n = len(t)
+
+        # dp[j] = number of ways to form t[:j]
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        for ch in s:
+            # Go backwards so dp[j - 1] is from the previous state
+            for j in range(n, 0, -1):
+                if ch == t[j - 1]:
+                    dp[j] += dp[j - 1]
+
+        return dp[n]
+
